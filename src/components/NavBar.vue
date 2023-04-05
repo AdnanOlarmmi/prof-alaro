c75d1f95c2fd3e8b71a9b0f878b430583e2ea229
AdnanOlarmmi <olarmmi@gmail.com>
2023-04-05 21:47:00 +0100

    Placed logo and menu bar properly

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,16 +1,20 @@
 <template>
     <v-app>
-      <v-app-bar app color="primary" dark>
+      <v-app-bar app color="black" dark>
     <!-- show nav only on mobile -->
+    <v-container class="d-flex ">
         <v-app-bar-nav-icon class="d-md-none" @click="drawer = !drawer"></v-app-bar-nav-icon>
 
     <!-- a link in vuetify -->
-        <v-toolbar-title><v-img
+        <v-btn href="/" class="no-hover"><v-img
         contain
         height="50"
         width="50"
         src="@/assets/logo.png"
-      /></v-toolbar-title>
+        alt="logo" class="no-hover"
+      /></v-btn>
+    </v-container>
+
         <v-spacer></v-spacer>
         <div class="d-none d-md-flex">
         <v-btn text>Home</v-btn>

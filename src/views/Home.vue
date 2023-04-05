030ae6cb588b3fa43271ca43f7c3530edd331b89
AdnanOlarmmi <olarmmi@gmail.com>
2023-04-05 14:03:56 +0100

    Rest

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,11 @@
+
+
 <template>
-  <Nav />
   <HelloWorld />
 </template>
 
 <script lang="ts" setup>
   import HelloWorld from '@/components/HelloWorld.vue'
-  import Nav from '@/components/nav.vue'
+import vuetify from 'vite-plugin-vuetify';
+import { createVuetify } from 'vuetify';
 </script>

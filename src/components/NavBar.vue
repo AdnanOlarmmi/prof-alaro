a955e9c20d56903ca7546b8a4bbb86cd9227fb10
AdnanOlarmmi <olarmmi@gmail.com>
2023-04-05 15:26:46 +0100

    Fix nav responsivity

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,16 +2,19 @@
     <v-app>
       <v-app-bar app color="primary" dark>
     <!-- show nav only on mobile -->
-        <v-app-bar-nav-icon class="d-lg-none" @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-app-bar-nav-icon class="d-md-none" @click="drawer = !drawer"></v-app-bar-nav-icon>
 
 
         <v-toolbar-title>My App</v-toolbar-title>
         <v-spacer></v-spacer>
+        <div class="d-none d-md-flex">
         <v-btn text>Home</v-btn>
         <v-btn text>About</v-btn>
         <v-btn text>Contact</v-btn>
+        </div>
+        
       </v-app-bar>
-      <v-navigation-drawer v-model="drawer" app>
+      <v-navigation-drawer v-model="drawer" class="d-md-none" app>
         <v-list>
           <v-list-item v-for="(item, index) in items" :key="index" @click="drawer = false">
             <v-list-item-title>{{ item.title }}</v-list-item-title>
